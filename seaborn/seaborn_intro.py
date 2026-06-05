faea719c20eefb4e50ec36ca53d8b856045872b4
Disha Singh <dishasingghh@gmail.com>
2026-06-05 14:47:57 +0530

    seaborn

--- seaborn/seaborn_intro.py
+++ seaborn/seaborn_intro.py
@@ -1,4 +1,15 @@
 import seaborn as sns;
 import pandas as pd;
 import numpy as np;
+import matplotlib.pyplot as plt;
 sns.get_dataset_names()
+train=pd.read_csv(r"C:\Users\disha\Downloads\train.csv")
+df=pd.DataFrame(train)
+# print(df["Age"].value_counts())
+# print(df.columns)
+sns.scatterplot(x="Age",y="Fare",data=df,hue="Sex",palette="dark")
+sns.set_context("paper",font_scale=1.5)
+plt.xlabel("Age")
+plt.ylabel("Fare")
+
+plt.show()
